2a3c9c8960c4bc9116ba83afe60ade840c388acc
izzy2lost <izzynochill@gmail.com>
2025-06-29 07:48:31 +0000

    Deploying to gh-pages from @ izzy2lost/SpaghettiKart@956058a32dfa3f26a20bc65fa83ac916fde073ab 🚀

---
+++ search/pages_e.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['spagettikart_0',['Building SpagettiKart',['../md_docs_2BUILDING.html',1,'']]],
+  ['surface_20types_1',['Surface Types',['../surfacetypes.html',1,'coursesmenu']]]
+];
