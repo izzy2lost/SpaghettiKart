1de35251f8221c6fe907b8b52ea391991eca533f
izzy2lost <izzynochill@gmail.com>
2025-07-01 00:13:14 -0400

    edit libultraship for android

---
+++ libultraship/include/libultraship/bridge.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "public/bridge/resourcebridge.h"
+#include "public/bridge/audiobridge.h"
+#include "public/bridge/controllerbridge.h"
+#include "public/bridge/windowbridge.h"
+#include "public/bridge/consolevariablebridge.h"
+#include "public/bridge/crashhandlerbridge.h"
+#include "public/bridge/gfxdebuggerbridge.h"
+#include "public/bridge/gfxbridge.h"
