2c237e6e2c62ce72920811101f6806b87713510d
kekko_dos <monacof81@gmail.com>
2025-09-13 13:10:27 +0200

    more AndroidTV changes

--- android/app/src/main/java/com/izzy/kart/TVActivity.java
+++ android/app/src/main/java/com/izzy/kart/TVActivity.java
@@ -9,24 +9,23 @@ public class TVActivity extends MainActivity{
         return true;
     }
 
-	@Override
-	public boolean dispatchKeyEvent(KeyEvent event) {
-		if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
-			int source = event.getSource();
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            int source = event.getSource();
 
-			boolean isKeyboard = (source & InputDevice.SOURCE_KEYBOARD) == InputDevice.SOURCE_KEYBOARD;
-			boolean isDpad = (source & InputDevice.SOURCE_DPAD) == InputDevice.SOURCE_DPAD;
-			boolean isGamepad = (source & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD;
+            boolean isDpad = (source & InputDevice.SOURCE_DPAD) == InputDevice.SOURCE_DPAD;
+            boolean isGamepad = (source & InputDevice.SOURCE_GAMEPAD) == InputDevice.SOURCE_GAMEPAD;
 
-			if (isKeyboard && !isDpad && !isGamepad) {
-				if (event.getAction() == KeyEvent.ACTION_UP) {
-					finish();
-					android.os.Process.killProcess(android.os.Process.myPid());
-				}
-				return true;
-			}
-			return false;
-		}
-		return super.dispatchKeyEvent(event);
-	}
+            if (isDpad && !isGamepad) {
+                if (event.getAction() == KeyEvent.ACTION_UP) {
+                    finish();
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                }
+                return true;
+            }
+            return false;
+        }
+        return super.dispatchKeyEvent(event);
+    }
 }
